1636963aeb48922c7b9bb52659c0d31e98903310
Pilar Ormeño <pilar.ormeno.reyes@gmail.com>
2021-06-27 15:28:44 -0400

    Examen
    Example of pre filter

---
+++ vetzuul/src/main/java/com/escalab/practice/zuul/server/PreFilter.java
@@ -0,0 +1,48 @@
+package com.escalab.practice.zuul.server;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * The Class PreFilter.
+ */
+public class PreFilter extends ZuulFilter {
+	
+	/** The Constant FILTERTYPE. */
+	private static final String FILTERTYPE = "pre";
+    
+    /** The Constant FILTERORDER. */
+    private static final Integer FILTERORDER = 1;
+    
+    private static Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		final HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		log.info("Petición {} a {}", request.getMethod(), request.getRequestURL().toString());
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return FILTERTYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return FILTERORDER;
+	}
+
+}
--- vetzuul/src/main/java/com/escalab/practice/zuul/server/ZuulServerApplication.java
+++ vetzuul/src/main/java/com/escalab/practice/zuul/server/ZuulServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +13,11 @@ public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
 	}
 
 }
