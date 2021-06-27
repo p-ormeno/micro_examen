bdb25aa238ac41709854c119083565a576acbf4e
Pilar Ormeño <pilar.ormeno.reyes@gmail.com>
2021-06-27 18:05:12 -0400

    Examen
    Zuul fallback response

--- vetzuul/src/main/java/com/escalab/practice/zuul/server/fallback/PreFallBackProvider.java
+++ vetzuul/src/main/java/com/escalab/practice/zuul/server/fallback/PreFallBackProvider.java
@@ -2,10 +2,20 @@ package com.escalab.practice.zuul.server.fallback;
 
 import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 
+import com.escalab.practice.zuul.server.reponse.GatewayClientResponse;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+
+/**
+ * The Class PreFallBackProvider.
+ */
 @Configuration
 public class PreFallBackProvider implements FallbackProvider {
+	
+	/** The Constant DEFAULT_MESSAGE. */
+	private static final String DEFAULT_MESSAGE = "Servicio no disponible";
 
 	@Override
 	public String getRoute() {
@@ -14,8 +24,11 @@ public class PreFallBackProvider implements FallbackProvider {
 
 	@Override
 	public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
-		// TODO Auto-generated method stub
-		return null;
+		if (cause instanceof HystrixTimeoutException) {
+            return new GatewayClientResponse(HttpStatus.GATEWAY_TIMEOUT, DEFAULT_MESSAGE);
+        } else {
+            return new GatewayClientResponse(HttpStatus.INTERNAL_SERVER_ERROR, DEFAULT_MESSAGE);
+        }
 	}
 
 }
---
+++ vetzuul/src/main/java/com/escalab/practice/zuul/server/reponse/GatewayClientResponse.java
@@ -0,0 +1,75 @@
+package com.escalab.practice.zuul.server.reponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+/**
+ * The Class GatewayClientResponse.
+ */
+public class GatewayClientResponse implements ClientHttpResponse {
+	
+	/** The status. */
+	private HttpStatus status;
+    
+    /** The message. */
+    private String message;
+
+    public GatewayClientResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getStatusCode() throws IOException {
+        return status;
+    }
+
+    @Override
+    public int getRawStatusCode() throws IOException {
+        return status.value();
+    }
+
+    @Override
+    public String getStatusText() throws IOException {
+        return status.getReasonPhrase();
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public InputStream getBody() throws IOException {
+        return new ByteArrayInputStream(message.getBytes());
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
